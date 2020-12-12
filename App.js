f668b61af9deac7b4a52a3c4b1b80756c9f4a176
arzuustun <ustunarzu06@gmail.com>
2020-12-12 07:28:08 -0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Platform,StatusBar } from 'react-native';
+import AddEntry from './components/AddEntry';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import History from './components/History';
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons' 
+import Constants from 'expo-constants';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
-export default function App() {
+ const Tab = Platform.OS === 'ios'
+          ? createBottomTabNavigator() 
+          : createMaterialBottomTabNavigator()
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
+  )
+  }
+  export default class App extends React.Component {
+    render() {
+      return (
+        <Provider store={createStore(reducer)}>
+          <View style={{flex: 1}}>
+            <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+            <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            switch (route.name) {
+              case 'History':
+                return <Ionicons name="ios-bookmarks" size={size} color={color} />;
+              case 'AddEntry':
+                return <FontAwesome name="plus-square" size={size} color={color} />;
+              // case 'Live':
+              //   return (
+              //     <Ionicons name="ios-speedometer" size={size} color={color} />
+              //   );
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+       
+        <Tab.Screen name="History" component={History} />
+        <Tab.Screen name="AddEntry" component={AddEntry} />
+      </Tab.Navigator>
+    </NavigationContainer>
+          </View>
+        </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
---
+++ Appflexbox.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, AppRegistry } from 'react-native'
+
+class FlexboxExamples extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'red'}} />
+      <View style={{flex: 2, backgroundColor: 'green'}} />
+      <View style={{flex: 3, backgroundColor: 'blue'}} />
+    </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // alignItems: 'center',
+    // justifyContent:'center',
+    // flexDirection: 'row',
+    display: "flex",
+    flexDirection : "row",
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+  },
+  box: {
+   height: 50,
+    width: 50,
+    backgroundColor: '#e76e63',
+    margin: 10,
+  }
+})
+
+export default FlexboxExamples;
